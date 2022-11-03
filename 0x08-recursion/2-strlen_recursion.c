bcb79bb0a99cee1377d4b496e4fa050429892cba
Chicksman9 <okaforsamuel5252@gmail.com>
2022-11-03 04:34:47 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,8 +10,16 @@ int _strlen_recursion(char *s)
 {
 	int len = 0;
 
-	if (*s)
+	if (*s-- '\0')
+	{
+		return (0);
+	}
+	if (*s != '\0')
 	{
 		len++;
-		len += _strlen_recursion(s + 1);
+		len += _strlen_recursion(++s);
+		return (len);
 	}
+
+	return (0);
+}
